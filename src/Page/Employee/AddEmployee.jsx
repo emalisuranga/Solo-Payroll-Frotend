8c539910a7634a8861fefdb10a44ab560d58d84e
emalisuranga <rmeisuranga@gmail.com>
2024-07-10 17:02:47 +0900

    dynamic and containerized approach for your employee component

--- src/Page/Employee/AddEmployee.jsx
+++ src/Page/Employee/AddEmployee.jsx
@@ -9,115 +9,115 @@ const AddEmployee = () => {
     console.log(formData);
   };
 
-  // const sections = [
-  //   {
-  //     label: "Personal Info",
-  //     fields: [
-  //       {
-  //         name: "firstName",
-  //         type: "text",
-  //         label: "First Name",
-  //         required: true,
-  //       },
-  //       {
-  //         name: "lastName",
-  //         type: "text",
-  //         label: "Last Name",
-  //         required: true,
-  //       },
-  //       { name: "phone", type: "text", label: "Phone", required: true },
-  //       { name: "address", type: "text", label: "Address", required: true },
-  //       {
-  //         name: "dateOfBirth",
-  //         type: "date",
-  //         label: "Date of Birth",
-  //         required: true,
-  //       },
-  //       {
-  //         name: "joinDate",
-  //         type: "date",
-  //         label: "Join Date",
-  //         required: true,
-  //       },
-  //       {
-  //         name: "department",
-  //         type: "text",
-  //         label: "Department",
-  //         required: true,
-  //       },
-  //     ],
-  //   },
-  //   {
-  //     label: "Bank Details",
-  //     fields: [
-  //       {
-  //         name: "bankAccountNumber",
-  //         type: "text",
-  //         label: "Bank Account Number",
-  //         required: true,
-  //       },
-  //       {
-  //         name: "bankName",
-  //         type: "text",
-  //         label: "Bank Name",
-  //         required: true,
-  //       },
-  //       {
-  //         name: "branchCode",
-  //         type: "text",
-  //         label: "Branch Code",
-  //         required: true,
-  //       },
-  //     ],
-  //   },
-  //   {
-  //     label: "Salary Details",
-  //     fields: [
-  //       {
-  //         name: "basicSalary",
-  //         type: "text",
-  //         label: "Basic Salary",
-  //         required: true,
-  //       },
-  //       {
-  //         name: "overtimePay",
-  //         type: "text",
-  //         label: "Overtime Pay",
-  //         required: true,
-  //       },
-  //       {
-  //         name: "transportationCosts",
-  //         type: "text",
-  //         label: "Transportation Costs",
-  //         required: true,
-  //       },
-  //       {
-  //         name: "familyAllowance",
-  //         type: "text",
-  //         label: "Family Allowance",
-  //         required: true,
-  //       },
-  //       {
-  //         name: "attendanceAllowance",
-  //         type: "text",
-  //         label: "Attendance Allowance",
-  //         required: true,
-  //       },
-  //       {
-  //         name: "leaveAllowance",
-  //         type: "text",
-  //         label: "Leave Allowance",
-  //         required: true,
-  //       },
-  //       {
-  //         name: "specialAllowance",
-  //         type: "text",
-  //         label: "Special Allowance",
-  //         required: true,
-  //       },
-  //     ],
-  //   },
-  // ];
+  const sections = [
+    {
+      label: "Personal Info",
+      fields: [
+        {
+          name: "firstName",
+          type: "text",
+          label: "First Name",
+          required: true,
+        },
+        {
+          name: "lastName",
+          type: "text",
+          label: "Last Name",
+          required: true,
+        },
+        { name: "phone", type: "text", label: "Phone", required: true },
+        { name: "address", type: "text", label: "Address", required: true },
+        {
+          name: "dateOfBirth",
+          type: "date",
+          label: "Date of Birth",
+          required: true,
+        },
+        {
+          name: "joinDate",
+          type: "date",
+          label: "Join Date",
+          required: true,
+        },
+        {
+          name: "department",
+          type: "text",
+          label: "Department",
+          required: true,
+        },
+      ],
+    },
+    {
+      label: "Bank Details",
+      fields: [
+        {
+          name: "bankAccountNumber",
+          type: "text",
+          label: "Bank Account Number",
+          required: true,
+        },
+        {
+          name: "bankName",
+          type: "text",
+          label: "Bank Name",
+          required: true,
+        },
+        {
+          name: "branchCode",
+          type: "text",
+          label: "Branch Code",
+          required: true,
+        },
+      ],
+    },
+    {
+      label: "Salary Details",
+      fields: [
+        {
+          name: "basicSalary",
+          type: "text",
+          label: "Basic Salary",
+          required: true,
+        },
+        {
+          name: "overtimePay",
+          type: "text",
+          label: "Overtime Pay",
+          required: true,
+        },
+        {
+          name: "transportationCosts",
+          type: "text",
+          label: "Transportation Costs",
+          required: true,
+        },
+        {
+          name: "familyAllowance",
+          type: "text",
+          label: "Family Allowance",
+          required: true,
+        },
+        {
+          name: "attendanceAllowance",
+          type: "text",
+          label: "Attendance Allowance",
+          required: true,
+        },
+        {
+          name: "leaveAllowance",
+          type: "text",
+          label: "Leave Allowance",
+          required: true,
+        },
+        {
+          name: "specialAllowance",
+          type: "text",
+          label: "Special Allowance",
+          required: true,
+        },
+      ],
+    },
+  ];
 
   return (
     <React.Fragment>
@@ -130,7 +130,7 @@ const AddEmployee = () => {
         <BackButton />
         <Typography variant="h5">Register Form</Typography>
       </Stack>
-      <CustomTabs onSubmit={handleSubmit} />
+      <CustomTabs sections={sections} onSubmit={handleSubmit} />
     </React.Fragment>
   );
 };
